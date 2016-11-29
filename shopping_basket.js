266612ed862b74c441a4498a55afa6e9068ab5b8
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-29 16:44:25 +0000

    adding test for initial item count:

--- shopping_basket.js
+++ shopping_basket.js
@@ -2,6 +2,10 @@ var shoppingBasket = {
   totalPrice: function() {
 
     return 0.00;
+  },
+  itemCount: function() {
+
+    return 0;
   }
 };
 
--- specs/shopping_basket_specs.js
+++ specs/shopping_basket_specs.js
@@ -6,4 +6,8 @@ describe( "Shopping Basket", function() {
   it( "should start with totalPrice of 0.00", function() {
     assert.equal( 0.00, shoppingBasket.totalPrice() );
   });
+
+  it( "should start with itemCount of 0", function() {
+    assert.equal( 0, shoppingBasket.itemCount() );
+  });
 });
