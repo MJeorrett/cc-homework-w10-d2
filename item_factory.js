b182b6aa9de3f8ca0667af2f8830c9059d3ce668
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-29 16:34:04 +0000

    adding cost to items

--- item_factory.js
+++ item_factory.js
@@ -2,14 +2,20 @@ var arrayIncludes = require('array-includes')
 
 var itemFactory = {
   itemNames: [ "Bananas" ],
+  itemCosts: {
+    Bananas: 0.99
+  },
   getItem: function( itemName ) {
 
     var item = undefined;
 
     if ( arrayIncludes( this.itemNames, itemName ) ) {
 
+      var itemCost = this.itemCosts[itemName];
+
       item = {
-        name: itemName
+        name: itemName,
+        price: itemCost
       };
     }
 
