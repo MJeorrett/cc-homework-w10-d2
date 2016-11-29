7137dbc336c708f53f39e771f4a2ed664c57ceb6
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-29 18:06:09 +0000

    adding bogof values for other items

--- item_factory.js
+++ item_factory.js
@@ -3,10 +3,10 @@ var arrayIncludes = require('array-includes')
 var itemFactory = {
   items: {
     Bananas: { name: "Bananas", price: 0.99, bogof: false },
-    Cheese: { name: "Cheese", price: 5.00 },
-    Bacon: { name: "Bacon", price: 3.50 },
-    Sausages: { name: "Sausages", price: 4.15 },
-    Turkey: { name: "Turkey", price: 12.99 }
+    Cheese: { name: "Cheese", price: 5.00, bogof: true },
+    Bacon: { name: "Bacon", price: 3.50, bogof: true },
+    Sausages: { name: "Sausages", price: 4.15, bogof: false },
+    Turkey: { name: "Turkey", price: 12.99, bogof: false }
   },
   getItem: function( itemName ) {
 
--- specs/item_factory_spec.js
+++ specs/item_factory_spec.js
@@ -28,33 +28,41 @@ describe( "ItemFactory", function() {
     assert.equal( 0.99, bananas.price );
   })
 
-  it( "should return object with bogof === false when asked for Bananas", function() {
+  it( "should return object with bogof = false when asked for Bananas", function() {
     var result = bananas.bogof;
     assert.equal( 'boolean', typeof( result ) );
     assert.equal( false, result );
   });
 
-  it( "should return object with name 'Cheese' and price 5.00 when asked for 'Cheese'", function() {
+  it( "should return object with name 'Cheese', bogof = true and price 5.00 when asked for 'Cheese'", function() {
     var cheese = itemFactory.getItem( "Cheese" );
     assert.equal( "Cheese", cheese.name );
     assert.equal( 5.00, cheese.price );
+    assert.equal( 'boolean', typeof( cheese.bogof ) );
+    assert.equal( true, cheese.bogof );
   });
 
-  it( "should return object with name 'Bacon' and price 3.50 when asked for 'Bacon'", function() {
-    var cheese = itemFactory.getItem( "Bacon" );
-    assert.equal( "Bacon", cheese.name );
-    assert.equal( 3.50, cheese.price );
+  it( "should return object with name 'Bacon', bogof = true and price 3.50 when asked for 'Bacon'", function() {
+    var bacon = itemFactory.getItem( "Bacon" );
+    assert.equal( "Bacon", bacon.name );
+    assert.equal( 3.50, bacon.price );
+    assert.equal( 'boolean', typeof( bacon.bogof ) );
+    assert.equal( true, bacon.bogof );
   });
 
-  it( "should return object with name 'Sausages' and price 4.15 when asked for 'Sausages'", function() {
-    var cheese = itemFactory.getItem( "Sausages" );
-    assert.equal( "Sausages", cheese.name );
-    assert.equal( 4.15, cheese.price );
+  it( "should return object with name 'Sausages', bogof = false and price 4.15 when asked for 'Sausages'", function() {
+    var sausages = itemFactory.getItem( "Sausages" );
+    assert.equal( "Sausages", sausages.name );
+    assert.equal( 4.15, sausages.price );
+    assert.equal( 'boolean', typeof( sausages.bogof ) );
+    assert.equal( false, sausages.bogof );
   });
 
-  it( "should return object with name 'Turkey' and price 12.99 when asked for 'Turkey'", function() {
-    var cheese = itemFactory.getItem( "Turkey" );
-    assert.equal( "Turkey", cheese.name );
-    assert.equal( 12.99, cheese.price );
+  it( "should return object with name 'Turkey', bogof = false and price 12.99 when asked for 'Turkey'", function() {
+    var turkey = itemFactory.getItem( "Turkey" );
+    assert.equal( "Turkey", turkey.name );
+    assert.equal( 12.99, turkey.price );
+    assert.equal( 'boolean', typeof( turkey.bogof ) );
+    assert.equal( false, turkey.bogof );
   });
 });
