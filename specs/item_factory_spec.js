4e4a930d5edfff9ba618ec447b6eeca68a8d0ba0
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-29 16:23:25 +0000

    adding test

--- specs/item_factory_spec.js
+++ specs/item_factory_spec.js
@@ -10,6 +10,11 @@ describe( "ItemFactory", function() {
     bananas = itemFactory.getItem( "Bananas" );
   });
 
+  it( "should return an object when passed name is recognised", function() {
+    var actual = itemFactory.getItem( "Bananas" );
+    assert.equal( "object", typeof( actual ) );
+  });
+
   it( "bananas should have name bananas", function() {
 
     assert.equal( "Bananas", bananas.name );
