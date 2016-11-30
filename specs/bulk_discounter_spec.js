05652bcb453628c37b15e1d13423792f33ee01f7
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-30 09:29:47 +0000

    adding test for when discount should not be added

--- specs/bulk_discounter_spec.js
+++ specs/bulk_discounter_spec.js
@@ -3,7 +3,11 @@ var bulk_discounter = require('../bulk_discounter');
 
 describe( "Discounter", function() {
 
-  it( "Should have an applyBulkDiscount method that takes 10% off if total is over £20", function() {
+  it( "should have an 'apply' method that takes 10% off if total is over 20.00", function() {
     assert.equal( 19.80, bulk_discounter.apply( 22.00 ) );
+  });
+
+  it( "should have an 'apply' method that returns the original total if it is <= 20.00", function() {
+    assert.equal( 20.00, bulk_discounter.apply( 20.00 ) );
   });
 });
